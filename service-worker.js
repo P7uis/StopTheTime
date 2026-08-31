a7e47de44916429db1ba682e58aae603bad9c4fd
Techlab Coornhert Lyceum <techlab@coornhert.nl>
2026-08-31 21:59:47 +0200

    Emphasize live clock button

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "stop-the-time-v45";
+const CACHE_NAME = "stop-the-time-v46";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=45",
-  "./app.js?v=45",
+  "./styles.css?v=46",
+  "./app.js?v=46",
   "./manifest.webmanifest",
   "./icons/icon.svg",
 ];
